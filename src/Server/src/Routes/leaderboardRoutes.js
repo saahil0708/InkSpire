02a840390799e1f80c62d58912bcfa74e01805c5
Saahil <saahil.ind.dgp@gmail.com>
2026-02-25 22:58:10 +0530

    Add history endpoint to leaderboard routes

--- src/Server/src/Routes/leaderboardRoutes.js
+++ src/Server/src/Routes/leaderboardRoutes.js
@@ -11,5 +11,42 @@ router.get("/", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+// Get leaderboard history for graph
+router.get("/history", async (req, res) => {
+    try {
+        const Score = require("../Models/Score");
+        const Round = require("../Models/Round");
+
+        const rounds = await Round.find().sort({ createdAt: 1 });
+        const teams = await Team.find();
+
+        const history = [];
+
+        // We track cumulative scores as we iterate through rounds
+        let cumulativeScores = {};
+        teams.forEach(t => cumulativeScores[t._id.toString()] = 0);
+
+        for (const round of rounds) {
+            const scores = await Score.find({ roundId: round._id }).populate("teamId");
+            const dataPoint = { time: round.name };
+
+            scores.forEach(s => {
+                const teamName = s.teamId.name;
+                cumulativeScores[s.teamId._id.toString()] += s.scoreValue;
+            });
+
+            // Populate the dataPoint with current cumulative scores
+            teams.forEach(t => {
+                dataPoint[t.name] = cumulativeScores[t._id.toString()];
+            });
+
+            history.push(dataPoint);
+        }
+
+        res.json(history);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 module.exports = router;
